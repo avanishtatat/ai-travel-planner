d0e46779a53e42ccabd71bcb65bf551d2d2fbfe2
Avanish Tiwari <avanisht.at.at@gmail.com>
2026-06-22 12:53:55 +0530

    refactor(auth): improve validator message

--- backend/src/validators/authValidator.js
+++ backend/src/validators/authValidator.js
@@ -12,19 +12,22 @@ const handleValidationErrors = (req, res, next) => {
 
 const registerValidator = [
   body("name")
+    .trim()
     .notEmpty()
-    .withMessage("All fields are required")
+    .withMessage("Name is required")
     .isLength({ min: 2 })
     .withMessage("Name must be at least 2 characters long"),
   body("email")
+    .trim()
     .notEmpty()
-    .withMessage("All fields are required")
+    .withMessage("Email is required")
     .isEmail()
     .withMessage("Please provide a valid email address")
     .normalizeEmail(),
   body("password")
+    .trim()
     .notEmpty()
-    .withMessage("All fields are required")
+    .withMessage("Password is required")
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
   handleValidationErrors,
@@ -32,12 +35,16 @@ const registerValidator = [
 
 const loginValidator = [
   body("email")
+    .trim()
     .notEmpty()
-    .withMessage("All fields are required")
+    .withMessage("Email is required")
     .isEmail()
     .withMessage("Please provide a valid email address")
     .normalizeEmail(),
-  body("password").notEmpty().withMessage("All fields are required"),
+  body("password")
+    .trim()
+    .notEmpty()
+    .withMessage("Password is required"),
   handleValidationErrors,
 ];
 
